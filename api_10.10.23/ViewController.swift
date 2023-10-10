2978e45c9dec284ebdaa66613914f4cd9a130f82
ZalkarbekArapbaev <zalkarbek2022@gmail.com>
2023-10-10 12:53:32 +0600

    Initial Commit

---
+++ api_10.10.23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  api_10.10.23
+//
+//  Created by zalkarbek on 10/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
